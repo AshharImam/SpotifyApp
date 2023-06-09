6b07fa2d0cae87d5df0a7e5569da7ce3aa4659cb
ashharimam <ashharimam490@gmail.com>
2023-06-10 04:29:35 +0500

    folder structure setup

---
+++ src/navigations/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Navigations = () => {
+  return (
+    <View>
+      <Text>Navigations</Text>
+    </View>
+  );
+};
+
+export default Navigations;
---
+++ src/screens/TrackDetail/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const TrackDetail = () => {
+  return (
+    <View>
+      <Text>TrackDetail</Text>
+    </View>
+  );
+};
+
+export default TrackDetail;
